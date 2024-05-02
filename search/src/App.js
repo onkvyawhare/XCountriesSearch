fbfa85ecda9143ab62ed3b40c15b2457438699aa
onkvyawhare <onkvyawhare@gmail.com>
2024-05-02 21:03:00 +0530

    finall commit

--- search/src/App.js
+++ search/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import {useState,useEffect} from "react";
 import './App.css';
 
-function App() {
+function XCountriessearch() {
+
+
+  const [countries, setCountries] = useState([]);
+  const[searchTerm,setSearchterm]=useState("");
+
+  const handleSearch=(event)=>{
+    setSearchterm(event.target.value);
+
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error("Error fetching countries:", error);
+    }
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+    <h1>Countries Search</h1>
+    <input
+      type="text"
+      placeholder="Search for a country..."
+      value={searchTerm}
+      onChange={handleSearch}
+    />
+
+<div className="countries">
+        {filteredCountries.map((country) => (
+          <div className="countryCard" key={country.name.common}>
+            <img
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+            />
+            <p>{country.name.common}</p>
+          </div>
+        ))}
+        {filteredCountries.length === 0 && (
+          <p>No countries found matching the search term.</p>
+        )}
+      </div>
+      
     </div>
   );
 }
 
-export default App;
+export default XCountriessearch;
